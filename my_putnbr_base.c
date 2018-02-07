171623b576706af0738f4979ebd20eb5742b843e
philippe kam <censured@localhost>
2018-02-07 18:10:26 +0100

    my_putnbr_base

---
+++ my_putnbr_base.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2018
+** malloc
+** File description:
+** my_putnbr_base
+*/
+
+#include <unistd.h>
+#include "malloc.h"
+
+long int 		my_strlen(char *str)
+{
+	long int	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+long int 	my_putnbr_base(long int nbr, char *base)
+{
+	long int  len_base = my_strlen(base);
+	if (nbr >= len_base) {
+		my_putnbr_base((nbr / len_base), base);
+		write(1, &base[nbr % len_base], 1);
+	}
+	if (nbr < len_base && nbr > 0)
+		write(1, &base[nbr % len_base], 1);
+	return (nbr);
+}
---
+++ show_alloc_mem.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2021
+** PSU_2017_malloc
+** File description:
+** Created by ouranos27,
+*/
+#ifndef PSU_2017_MALLOC_SHOW_ALLOC_MEM_H
+#define PSU_2017_MALLOC_SHOW_ALLOC_MEM_H
+
+#endif /* !PSU_2017_MALLOC_SHOW_ALLOC_MEM_H*/
